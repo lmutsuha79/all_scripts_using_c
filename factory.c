46dfb0382f444d93cd309276875cc20c30640e71
lmutsuha79 <74467521+lmutsuha79@users.noreply.github.com>
2021-01-07 18:42:48 +0100

    Create factory.c

---
+++ factory.c
@@ -0,0 +1,35 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int num,x,result;
+    printf("Pleese Entre an number: "); scanf("%d",&num);
+    
+    if (num == 0){
+    	printf("0!=1");
+	}
+	else{
+		
+		result = num;
+
+		if (num < 0){
+			
+
+			num = -num;
+		}
+		x=num-1;
+		while (x >= 1){
+			result = result * x;
+			x--;
+		}
+		printf("factory = %d",result);	
+		
+	}
+    
+    
+    
+    
+    
+    return 0;
+}
